b2e85eeaee05d82111816f4b0b4d04f0130cfbe4
Anis Alkomem <anis.alkumaim@gmail.com>
2020-07-05 19:19:43 +0200

    Set-up main handles

--- src/App.js
+++ src/App.js
@@ -12,13 +12,65 @@ const initialExpenses = [
 ];
 // console.log(initialExpenses);
 function App() {
+    // ******************** state values **********************
+    // all expenses , add expense
     const [expenses, setExpenses] = useState(initialExpenses);
+    // single expense
+    const [charge, setCharge] = useState("");
+    // single amount
+    const [amount, setAmount] = useState("");
+    // alert
+    const [alert, setAlert] = useState({show: false});
+    // ******************** functionality **********************
+    // Handle charge
+    const handleCharge = (e) => {
+        setCharge(e.target.value);
+    };
+    // Handle amount
+    const handleAmount = (e) => {
+        setAmount(e.target.value);
+    };
+    // Handle alert
+    const handleAlert = ({type, text}) => {
+        setAlert({
+            show: true,
+            type,
+            text,
+        });
+        setTimeout(() => {
+            setAlert({show: false});
+        }, 2000);
+    };
+    // handle submit
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (charge !== "" && amount > 0) {
+            const singleExpense = {id: uuidv4, charge, amount};
+            setExpenses([...expenses, singleExpense]);
+            setCharge("");
+            setAmount("");
+            handleAlert({type: "success", text: "Item Added"});
+        } else {
+            handleAlert({
+                type: "danger",
+                text: "Charge can't be empty and/or amount can't be zero",
+            });
+        }
+    };
+
     return (
         <>
-            <Alert />
+            {alert.show && <Alert type={alert.type} text={alert.text} />}
+
             <h1>React Budget Calulator</h1>
             <main className="App">
-                <ExpenseForm />
+                <ExpenseForm
+                    charge={charge}
+                    amount={amount}
+                    handleCharge={handleCharge}
+                    handleAmount={handleAmount}
+                    handleSubmit={handleSubmit}
+                />
                 <ExpenseList expenses={expenses} />
             </main>
             <h1>
@@ -26,7 +78,7 @@ function App() {
                 <span className="total">
                     €{" "}
                     {expenses.reduce((acc, curr) => {
-                        return (acc += curr.amount);
+                        return (acc += parseInt(curr.amount));
                     }, 0)}
                 </span>
             </h1>
